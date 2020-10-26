7d95fbc86cf6c8e38f27c5a146f1bcd959476580
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-10-26 10:27:57 +0700

    create root query

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const schema = require('./schema/schema')
 const { graphqlHTTP } = require('express-graphql')
+
+
 const app = express()
 
 app.use('/graphql', graphqlHTTP({
-    graphiql: true,
+    schema,
+    graphiql: true
 }))
 
 app.listen(4000, () => {
--- schema/schema.js
+++ schema/schema.js
@@ -1,11 +1,28 @@
 const graphql = require('graphql')
-const { GraphQLObjectType, GraphQLString } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
 
 const jurusanType = new GraphQLObjectType({
     name: 'jurusan',
-    fiels: () => ({
+    fields: () => ({
         id: { type: GraphQLString },
         jurusan: { type: GraphQLString },
         kaprodi: { type: GraphQLString }
     })
+})
+
+const rootQuery = new GraphQLObjectType({
+    name: 'RootQueryTypr',
+    fields: {
+         prodi: {
+             type: jurusanType,
+             args: { id: { type: GraphQLString }},
+             resolve(parent, args) {
+
+             }
+         }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: rootQuery
 })
