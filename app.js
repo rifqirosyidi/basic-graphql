8307b743c7627cdf137f3413cde00d813526aff1
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-10-26 15:12:19 +0700

    connect to mongodb atlas

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+require('dotenv').config()
+
 const express = require('express')
+const monggose = require('mongoose')
 const schema = require('./schema/schema')
 const { graphqlHTTP } = require('express-graphql')
 
-
 const app = express()
+
+monggose.connect(process.env.MONGO_DB_CONN, {useNewUrlParser: true, useUnifiedTopology: true})
+monggose.connection.once('open', () => {
+    console.log("DB Connected")
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
